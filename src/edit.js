838fb9ed90ad17ca4e1007e2bec35ed7404475c8
Mario Aguiar <mario.aguiar@xwp.co>
2023-07-21 13:41:36 -0600

    Add inspector controls and content richtext

--- src/edit.js
+++ src/edit.js
@@ -4,6 +4,11 @@
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
  */
 import { __ } from '@wordpress/i18n';
+import { InspectorControls, RichText } from '@wordpress/block-editor';
+import {
+	PanelBody,
+	SelectControl,
+} from '@wordpress/components';
 
 /**
  * React hook that is used to mark the block wrapper element.
@@ -20,6 +25,7 @@ import { useBlockProps } from '@wordpress/block-editor';
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
 import './editor.scss';
+import metadata from './block.json';
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -29,10 +35,36 @@ import './editor.scss';
  *
  * @return {WPElement} Element to render.
  */
-export default function Edit() {
+export default function Edit( { attributes, setAttributes } ) {
+	const types = metadata.attributes.type.enum;
+	const blockProps = useBlockProps( {
+		className: `mah-asides--${ attributes.type }`,
+	} );
+
 	return (
-		<p { ...useBlockProps() }>
-			{ __( 'Mah Asides – hello from the editor!', 'mah-asides' ) }
-		</p>
+		<>
+			<InspectorControls>
+				<PanelBody title={ __( 'Type', 'mah-asides' ) }>
+					<SelectControl
+						label={ __( 'Type', 'mah-asides' ) }
+						value={ attributes.type }
+						options={
+							types.map( ( type ) => ( {
+								label: type.charAt( 0 ).toUpperCase() + type.slice( 1 ),
+								value: type
+						} ) ) }
+						onChange={ ( type ) => setAttributes( { type } ) }
+					/>
+                </PanelBody>
+			</InspectorControls>
+
+			<RichText
+				tagName="p"
+				placeholder={ __( 'Mah Asides – hello from the editor!', 'mah-asides' ) }
+				value={ attributes.content }
+				onChange={ ( content ) => setAttributes( { content } ) }
+				{ ...blockProps }
+			/>
+		</>
 	);
 }
