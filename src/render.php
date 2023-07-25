8868b28f2e8a14347b54176326256e20486e9c74
Mario Aguiar <mario.aguiar@xwp.co>
2023-07-25 12:06:24 -0600

    Render aside

--- src/render.php
+++ src/render.php
@@ -3,6 +3,11 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Mah Asides – hello from a dynamic block!', 'mah-asides' ); ?>
-</p>
+
+<aside <?php echo get_block_wrapper_attributes(); ?>>
+	<h3 className="mah-asides__title">
+		<?php echo esc_html( ucfirst( $attributes['type'] ) ); ?>
+	</h3>
+
+	<?php echo wp_kses_post( $attributes['content'] ); ?>
+</aside>
