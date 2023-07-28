492ec3d4081bc8593c5692280cabe9027cd67b8a
Mario Aguiar <mario.aguiar@xwp.co>
2023-07-28 13:18:25 -0600

    Add color classes

--- src/render.php
+++ src/render.php
@@ -2,9 +2,17 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
+
+$classes_wrapper = [
+	'mah-asides',
+	sprintf( 'mah-asides--%s', $attributes['type'] ),
+];
 ?>
 
-<aside <?php echo get_block_wrapper_attributes(); ?>>
+
+<aside <?php echo get_block_wrapper_attributes( [
+	'class' => implode(' ', $classes_wrapper ),
+] ); ?>>
 	<h3 className="mah-asides__title">
 		<?php echo esc_html( ucfirst( $attributes['type'] ) ); ?>
 	</h3>
